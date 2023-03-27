e61504c4bb8b2dbb83341ca73d9532122e69702b
남연진 <ygeenee@naver.com>
2023-03-28 03:09:50 +0900

    back

---
+++ backend/src/main/java/sinemuscat/SineMuscat/SineMuscatApplication.java
@@ -0,0 +1,13 @@
+package sinemuscat.SineMuscat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SineMuscatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SineMuscatApplication.class, args);
+	}
+
+}
