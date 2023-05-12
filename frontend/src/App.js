16dd13dd9a10f5252c574701bb103c33c40b070d
wkkyu088 <dnjsrbwls08@gmail.com>
2023-05-12 18:15:06 +0900

    항시 로그인 되어있게 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,8 +17,9 @@ import ProductDetailPage from './pages/ProductDetailPage';
 import PurchaseResultPage from './pages/PurchaseResultPage';
 
 function App() {
-  // console.log(sessionStorage.getItem('userId'))
-  // sessionStorage.clear()
+  // 아이디 'dnjsrbwls'으로 항시 로그인
+  sessionStorage.setItem('userId', 'dnjsrbwls');
+  sessionStorage.setItem('userColor', '#'+Math.floor(Math.random()*16777215).toString(16));
 
   return (
     <>
