9ee4ed482859508ddb47e1956080b3707e8c9322
ahn1234 <71892651+ahn1234@users.noreply.github.com>
2023-05-30 01:35:36 +0900

    go코드 리액트코드와 통신을 위한 코드 수정 및 설정

--- golang/src/main.go
+++ golang/src/main.go
@@ -2,50 +2,87 @@ package main
 
 import (
 	"context"
-	"fmt"
-	cdp "github.com/chromedp/chromedp"
+	"encoding/json"
+	"github.com/chromedp/chromedp"
+	"github.com/rs/cors"
 	"log"
+	"net/http"
 	"time"
 )
 
-func sendRequest(url, username, issuenumber string) {
+type Certificate struct {
+	Number string `json:"number"`
+}
+
+func SendRequest(url, username, issuenumber string) string {
 	selname := `//input[@id="schVltrNm"]`                        // 이름
 	selissu := `//input[@id="schIssuNo"]`                        // 발급번호
 	restitle := `//div[@class="board_list board_list2 non_sub"]` // 봉사 내역
 
-	contextVar, cancelFunc := cdp.NewContext(
+	contextVar, cancelFunc := chromedp.NewContext(
 		context.Background(),
-		cdp.WithLogf(log.Printf),
+		chromedp.WithLogf(log.Printf),
 	)
 	defer cancelFunc()
 
 	contextVar, cancelFunc = context.WithTimeout(contextVar, 30*time.Second) // timeout 값을 설정
 	defer cancelFunc()
-	contextVar, cancelFunc = cdp.NewContext(contextVar)
+	contextVar, cancelFunc = chromedp.NewContext(contextVar)
 	defer cancelFunc()
 
 	var strVar string
 
-	err := cdp.Run(contextVar,
-		cdp.Navigate(url),
-		cdp.WaitVisible(selissu),
-		cdp.SendKeys(selname, username),
-		cdp.SendKeys(selissu, issuenumber),
-		cdp.Submit(selissu),
-		cdp.WaitVisible(restitle),
-		cdp.Text(restitle, &strVar, cdp.NodeVisible),
+	err := chromedp.Run(contextVar,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(selissu),
+		chromedp.SendKeys(selname, username),
+		chromedp.SendKeys(selissu, issuenumber),
+		chromedp.Submit(selissu),
+		chromedp.WaitVisible(restitle),
+		chromedp.Text(restitle, &strVar, chromedp.NodeVisible),
 	)
 	if err != nil {
-		panic(err)
+		log.Printf("Error during web scraping: %v\n", err)
+		return ""
 	}
 
-	fmt.Println(strVar)
+	return strVar
+}
 
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	var cert Certificate
+	err := json.NewDecoder(r.Body).Decode(&cert)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	const url = "https://www.1365.go.kr/vols/P9330/srvcgud/cnfrmnIssu.do"
+	const userName = "원규진"
+	result := SendRequest(url, userName, cert.Number)
+
+	cert.Number = result
+
+	// Log the response
+	log.Printf("Received request with certificate number: %s\n", cert.Number)
+
+	// Return the result
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(cert); err != nil {
+		log.Printf("Error encoding JSON response: %v\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	const url = "https://www.1365.go.kr/vols/P9330/srvcgud/cnfrmnIssu.do"
-	const userName = "원규진"
-	const userIssueNumber = "202321921438_28830864"
-	sendRequest(url, userName, userIssueNumber)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", handleRequest)
+
+	// Use the CORS middleware to handle CORS
+	handler := cors.Default().Handler(mux)
+
+	log.Println("Server listening on :8080")
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatalf("Server error: %v\n", err)
+	}
 }
